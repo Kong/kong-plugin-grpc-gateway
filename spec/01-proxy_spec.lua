9368d96174c963df0196cc0167b594840f842e4e
Javier Guerra <javier.guerra@konghq.com>
2020-10-05 22:36:23 -0500

    tests - add tests for buffered request and/or response

--- spec/01-proxy_spec.lua
+++ spec/01-proxy_spec.lua
@@ -11,80 +11,83 @@ local function be_bytes(x)
 end
 
 for _, strategy in helpers.each_strategy() do
+  for buffering_request, buffering_response, buffering_label in helpers.each_buffering() do
+    describe("gRPC-Gateway [#" .. strategy .. "](".. buffering_label ..")", function()
+      local proxy_client
+      local proxy_client_ssl
 
-  describe("gRPC-Gateway [#" .. strategy .. "]", function()
-    local proxy_client
-    local proxy_client_ssl
 
+      lazy_setup(function()
+        local bp = helpers.get_db_utils(strategy, {
+          "routes",
+          "services",
+          "plugins",
+        }, {
+          "grpc-gateway",
+        })
 
-    lazy_setup(function()
-      local bp = helpers.get_db_utils(strategy, {
-        "routes",
-        "services",
-        "plugins",
-      }, {
-        "grpc-gateway",
-      })
+        local service1 = assert(bp.services:insert {
+          name = "grpc",
+          url = "grpc://localhost:15002",
+        })
 
-      local service1 = assert(bp.services:insert {
-        name = "grpc",
-        url = "grpc://localhost:15002",
-      })
+        local route1 = assert(bp.routes:insert {
+          protocols = { "http", "https" },
+          paths = { "/" },
+          service = service1,
+          request_buffering = buffering_request,
+          response_buffering = buffering_response,
+        })
 
-      local route1 = assert(bp.routes:insert {
-        protocols = { "http", "https" },
-        paths = { "/" },
-        service = service1,
-      })
+        assert(bp.plugins:insert {
+          route = route1,
+          name = "grpc-gateway",
+          config = {
+            proto = "spec/fixtures/grpc/hello_gw.proto",
+          },
+        })
 
-      assert(bp.plugins:insert {
-        route = route1,
-        name = "grpc-gateway",
-        config = {
-          proto = "spec/fixtures/grpc/hello_gw.proto",
-        },
-      })
+        assert(helpers.start_kong {
+          database = strategy,
+          plugins = "bundled,grpc-gateway",
+        })
+      end)
 
-      assert(helpers.start_kong {
-        database = strategy,
-        plugins = "bundled,grpc-gateway",
-      })
+      before_each(function()
+        proxy_client = helpers.proxy_client(1000)
+        proxy_client_ssl = helpers.proxy_ssl_client(1000)
+      end)
+
+      lazy_teardown(function()
+        helpers.stop_kong()
+      end)
+
+      test("main entrypoint", function()
+        local res, err = proxy_client:get("/v1/messages/john_doe")
+        local data = cjson.decode((res:read_body()))
+
+        assert.equal(200, res.status)
+        assert.is_nil(err)
+
+        assert.same({reply = "hello john_doe"}, data)
+      end)
+
+      test("additional binding", function()
+        local res, err = proxy_client:get("/v1/messages/legacy/john_doe")
+        local data = cjson.decode((res:read_body()))
+
+        assert.equal(200, res.status)
+        assert.is_nil(err)
+
+        assert.same({reply = "hello john_doe"}, data)
+      end)
+
+      test("unknown path", function()
+        local res, err = proxy_client:get("/v1/messages/john_doe/bai")
+        assert.not_equal(200, res.status)
+        assert.equal("Bad Request", res.reason)
+      end)
+
     end)
-
-    before_each(function()
-      proxy_client = helpers.proxy_client(1000)
-      proxy_client_ssl = helpers.proxy_ssl_client(1000)
-    end)
-
-    lazy_teardown(function()
-      helpers.stop_kong()
-    end)
-
-    test("main entrypoint", function()
-      local res, err = proxy_client:get("/v1/messages/john_doe")
-      local data = cjson.decode((res:read_body()))
-
-      assert.equal(200, res.status)
-      assert.is_nil(err)
-
-      assert.same({reply = "hello john_doe"}, data)
-    end)
-
-    test("additional binding", function()
-      local res, err = proxy_client:get("/v1/messages/legacy/john_doe")
-      local data = cjson.decode((res:read_body()))
-
-      assert.equal(200, res.status)
-      assert.is_nil(err)
-
-      assert.same({reply = "hello john_doe"}, data)
-    end)
-
-    test("unknown path", function()
-      local res, err = proxy_client:get("/v1/messages/john_doe/bai")
-      assert.not_equal(200, res.status)
-      assert.equal("Bad Request", res.reason)
-    end)
-
- end)
+  end
 end
